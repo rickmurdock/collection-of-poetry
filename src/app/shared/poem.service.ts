bfdfbb51a989c5db52d0461dce70042843aa13ba
rickmurdock <rick.murdock@live.com>
2018-12-28 00:11:49 -0600

    Shared service for poems.

--- src/app/shared/poem.service.ts
+++ src/app/shared/poem.service.ts
@@ -1,36 +1,62 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { GENERALPOEMS } from './general-poems';
 import { PUBLISHEDPOEMS } from './published-poems';
 import { UNTITLEDPOEMS } from './untitled-poems';
 import { WORSHIPFULPOEMS } from './worshipful-poems';
+import { UntitledComponent } from '../untitled/untitled.component';
+import { UntitledPoem } from './untitled-poem.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoemService {
 
-  constructor() { }
+  private untitledUrl = 'api/untitledpoems';
+
+  constructor(private http: HttpClient) { }
 
   getGeneralPoem(name): Observable<object> {
-    let titledPoem = GENERALPOEMS.find(poem => poem.title == name);
+    const titledPoem = GENERALPOEMS.find(poem => poem.title === name);
     return of(titledPoem);
   }
 
-  getPublishedPoem(name): Observable<object> {
-    let titledPoem = PUBLISHEDPOEMS.find(poem => poem.title == name);
+  getPublishedPoem(name: string): Observable<object> {
+    const titledPoem = PUBLISHEDPOEMS.find(poem => poem.title === name);
     return of(titledPoem);
   }
 
   getUntitledPoem(id): Observable<string> {
-    let untitled = UNTITLEDPOEMS.find(poem => poem.id == id);
-    let res = untitled.poem;
+    const untitled = UNTITLEDPOEMS.find(poem => poem.id === id);
+    const res = untitled.poem;
     return of(res);
   }
 
+  //
+  getUntitledPoems(): Observable<UntitledPoem[]> {
+    console.log('1111');
+    // console.log(this.http.get<UntitledPoem[]>(this.untitledUrl))
+    return this.http.get<UntitledPoem[]>(this.untitledUrl)
+      .pipe(
+        catchError(this.handleError('untitledpoems', []))
+      );
+  }
+
   getWorshipfulPoem(name): Observable<object> {
-    let titledPoem = WORSHIPFULPOEMS.find(poem => poem.title == name);
+    const titledPoem = WORSHIPFULPOEMS.find(poem => poem.title === name);
     return of(titledPoem);
   }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.log('error-------------------', error);
+
+      return of(result as T);
+    };
+  }
+
 }
